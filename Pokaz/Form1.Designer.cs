f8900d27d682933d4ba84726e4ce3aa61feffd6b
Andrei Goncharov <andreigoncharov1009@gmail.com>
2019-04-18 11:22:49 +0300

    Добавьте файлы проекта.

---
+++ Pokaz/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace Pokaz
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.start = new System.Windows.Forms.Button();
+			this.rah = new System.Windows.Forms.Label();
+			this.kv64 = new System.Windows.Forms.CheckBox();
+			this.kv65 = new System.Windows.Forms.CheckBox();
+			this.FIO = new System.Windows.Forms.Label();
+			this.date = new System.Windows.Forms.TextBox();
+			this.q6 = new System.Windows.Forms.TextBox();
+			this.q4 = new System.Windows.Forms.TextBox();
+			this.q3 = new System.Windows.Forms.TextBox();
+			this.q5 = new System.Windows.Forms.TextBox();
+			this.q2 = new System.Windows.Forms.TextBox();
+			this.q11 = new System.Windows.Forms.TextBox();
+			this.q10 = new System.Windows.Forms.TextBox();
+			this.q9 = new System.Windows.Forms.TextBox();
+			this.q8 = new System.Windows.Forms.TextBox();
+			this.q7 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.showd = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.Color.Red;
+			this.label1.Location = new System.Drawing.Point(9, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(98, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Личный счет:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(195, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Фамилия:";
+			// 
+			// start
+			// 
+			this.start.Location = new System.Drawing.Point(293, 375);
+			this.start.Name = "start";
+			this.start.Size = new System.Drawing.Size(244, 56);
+			this.start.TabIndex = 4;
+			this.start.Text = "Пуск";
+			this.start.UseVisualStyleBackColor = true;
+			this.start.Click += new System.EventHandler(this.start_Click);
+			// 
+			// rah
+			// 
+			this.rah.AutoSize = true;
+			this.rah.ForeColor = System.Drawing.Color.Black;
+			this.rah.Location = new System.Drawing.Point(113, 56);
+			this.rah.Name = "rah";
+			this.rah.Size = new System.Drawing.Size(56, 17);
+			this.rah.TabIndex = 5;
+			this.rah.Text = "111111";
+			// 
+			// kv64
+			// 
+			this.kv64.AutoSize = true;
+			this.kv64.Checked = true;
+			this.kv64.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.kv64.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.kv64.Location = new System.Drawing.Point(12, 12);
+			this.kv64.Name = "kv64";
+			this.kv64.Size = new System.Drawing.Size(144, 21);
+			this.kv64.TabIndex = 6;
+			this.kv64.Text = "Для квартиры 64";
+			this.kv64.UseVisualStyleBackColor = true;
+			this.kv64.CheckedChanged += new System.EventHandler(this.kv64_CheckedChanged);
+			// 
+			// kv65
+			// 
+			this.kv65.AutoSize = true;
+			this.kv65.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+			this.kv65.Location = new System.Drawing.Point(198, 12);
+			this.kv65.Name = "kv65";
+			this.kv65.Size = new System.Drawing.Size(144, 21);
+			this.kv65.TabIndex = 7;
+			this.kv65.Text = "Для квартиры 65";
+			this.kv65.UseVisualStyleBackColor = true;
+			this.kv65.CheckedChanged += new System.EventHandler(this.kv65_CheckedChanged);
+			// 
+			// FIO
+			// 
+			this.FIO.AutoSize = true;
+			this.FIO.ForeColor = System.Drawing.Color.Black;
+			this.FIO.Location = new System.Drawing.Point(275, 56);
+			this.FIO.Name = "FIO";
+			this.FIO.Size = new System.Drawing.Size(56, 17);
+			this.FIO.TabIndex = 8;
+			this.FIO.Text = "222222";
+			// 
+			// date
+			// 
+			this.date.Location = new System.Drawing.Point(7, 171);
+			this.date.Name = "date";
+			this.date.Size = new System.Drawing.Size(155, 22);
+			this.date.TabIndex = 9;
+			// 
+			// q6
+			// 
+			this.q6.Location = new System.Drawing.Point(668, 171);
+			this.q6.Name = "q6";
+			this.q6.Size = new System.Drawing.Size(119, 22);
+			this.q6.TabIndex = 10;
+			// 
+			// q4
+			// 
+			this.q4.Location = new System.Drawing.Point(418, 171);
+			this.q4.Name = "q4";
+			this.q4.Size = new System.Drawing.Size(119, 22);
+			this.q4.TabIndex = 11;
+			// 
+			// q3
+			// 
+			this.q3.Location = new System.Drawing.Point(293, 171);
+			this.q3.Name = "q3";
+			this.q3.Size = new System.Drawing.Size(119, 22);
+			this.q3.TabIndex = 12;
+			// 
+			// q5
+			// 
+			this.q5.Location = new System.Drawing.Point(543, 171);
+			this.q5.Name = "q5";
+			this.q5.Size = new System.Drawing.Size(119, 22);
+			this.q5.TabIndex = 13;
+			// 
+			// q2
+			// 
+			this.q2.Location = new System.Drawing.Point(168, 171);
+			this.q2.Name = "q2";
+			this.q2.Size = new System.Drawing.Size(119, 22);
+			this.q2.TabIndex = 14;
+			// 
+			// q11
+			// 
+			this.q11.Location = new System.Drawing.Point(606, 245);
+			this.q11.Name = "q11";
+			this.q11.Size = new System.Drawing.Size(119, 22);
+			this.q11.TabIndex = 15;
+			// 
+			// q10
+			// 
+			this.q10.Location = new System.Drawing.Point(481, 245);
+			this.q10.Name = "q10";
+			this.q10.Size = new System.Drawing.Size(119, 22);
+			this.q10.TabIndex = 16;
+			// 
+			// q9
+			// 
+			this.q9.Location = new System.Drawing.Point(356, 245);
+			this.q9.Name = "q9";
+			this.q9.Size = new System.Drawing.Size(119, 22);
+			this.q9.TabIndex = 17;
+			// 
+			// q8
+			// 
+			this.q8.Location = new System.Drawing.Point(231, 245);
+			this.q8.Name = "q8";
+			this.q8.Size = new System.Drawing.Size(119, 22);
+			this.q8.TabIndex = 18;
+			// 
+			// q7
+			// 
+			this.q7.Location = new System.Drawing.Point(106, 245);
+			this.q7.Name = "q7";
+			this.q7.Size = new System.Drawing.Size(119, 22);
+			this.q7.TabIndex = 19;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(91, 141);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(16, 17);
+			this.label3.TabIndex = 21;
+			this.label3.Text = "1";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(228, 141);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(16, 17);
+			this.label4.TabIndex = 22;
+			this.label4.Text = "2";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(334, 141);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(16, 17);
+			this.label5.TabIndex = 23;
+			this.label5.Text = "3";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(459, 141);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(16, 17);
+			this.label6.TabIndex = 24;
+			this.label6.Text = "4";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(584, 141);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(16, 17);
+			this.label7.TabIndex = 25;
+			this.label7.Text = "5";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(709, 141);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(16, 17);
+			this.label8.TabIndex = 26;
+			this.label8.Text = "6";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(153, 225);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(16, 17);
+			this.label9.TabIndex = 27;
+			this.label9.Text = "7";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(290, 225);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(16, 17);
+			this.label10.TabIndex = 28;
+			this.label10.Text = "8";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(415, 225);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(16, 17);
+			this.label11.TabIndex = 29;
+			this.label11.Text = "9";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(540, 225);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(24, 17);
+			this.label12.TabIndex = 30;
+			this.label12.Text = "10";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(638, 225);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(24, 17);
+			this.label13.TabIndex = 31;
+			this.label13.Text = "11";
+			// 
+			// showd
+			// 
+			this.showd.AutoSize = true;
+			this.showd.Location = new System.Drawing.Point(557, 56);
+			this.showd.Name = "showd";
+			this.showd.Size = new System.Drawing.Size(105, 17);
+			this.showd.TabIndex = 32;
+			this.showd.Text = "Текущая дата:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.ClientSize = new System.Drawing.Size(800, 443);
+			this.Controls.Add(this.showd);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.q7);
+			this.Controls.Add(this.q8);
+			this.Controls.Add(this.q9);
+			this.Controls.Add(this.q10);
+			this.Controls.Add(this.q11);
+			this.Controls.Add(this.q2);
+			this.Controls.Add(this.q5);
+			this.Controls.Add(this.q3);
+			this.Controls.Add(this.q4);
+			this.Controls.Add(this.q6);
+			this.Controls.Add(this.date);
+			this.Controls.Add(this.FIO);
+			this.Controls.Add(this.kv65);
+			this.Controls.Add(this.kv64);
+			this.Controls.Add(this.rah);
+			this.Controls.Add(this.start);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(818, 490);
+			this.MinimumSize = new System.Drawing.Size(818, 490);
+			this.Name = "Form1";
+			this.Text = "Показания";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button start;
+		private System.Windows.Forms.Label rah;
+		private System.Windows.Forms.CheckBox kv64;
+		private System.Windows.Forms.CheckBox kv65;
+		private System.Windows.Forms.Label FIO;
+		private System.Windows.Forms.TextBox date;
+		private System.Windows.Forms.TextBox q6;
+		private System.Windows.Forms.TextBox q4;
+		private System.Windows.Forms.TextBox q3;
+		private System.Windows.Forms.TextBox q5;
+		private System.Windows.Forms.TextBox q2;
+		private System.Windows.Forms.TextBox q11;
+		private System.Windows.Forms.TextBox q10;
+		private System.Windows.Forms.TextBox q9;
+		private System.Windows.Forms.TextBox q8;
+		private System.Windows.Forms.TextBox q7;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label showd;
+	}
+}
+
---
+++ Pokaz/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using System.Net;
+
+namespace Pokaz
+{
+	public partial class Form1 : Form
+	{
+		string rah64 = "619100178"; string rah65 = "619100179";
+		string FIO64 = "Гончарова"; string FIO65 = "Гончаров";
+		public Form1()
+		{
+			InitializeComponent();
+
+			if (kv64.Checked == true)
+			{
+				rah.Text = rah64;
+				FIO.Text = FIO64;
+			}
+			else if (kv65.Checked == true)
+			{
+				rah.Text = rah65;
+				FIO.Text = FIO65;
+			}
+			string dnow = DateTime.Now.ToString();
+			date.Text = dnow;
+			q11.Text = "F-";
+			string dnow1 = DateTime.Now.ToString("dd:MM:yyyy");
+			showd.Text = ("Текущая дата: ") + dnow1;
+		}
+
+		private void Form1_Load(object sender, EventArgs e) { }
+
+		private void start_Click(object sender, EventArgs e)
+		{
+			if (!conection())
+			{
+				MessageBox.Show("Нет соеденения с интернетом. Проверьте соеденение с интернетом.");
+				return;
+			}
+
+			IWebDriver web;
+			string Rah = rah.Text;
+			string fio = FIO.Text;
+			web = new ChromeDriver();
+			try
+			{
+				web.Navigate().GoToUrl("https://www.hts.kharkov.ua/KPHTS_v2_bill1PU.php");
+
+				if (kv64.Checked == true)
+				{
+					web.FindElement(By.XPath("//input[@id='acc-input']")).SendKeys(rah64);
+				}
+				else if (kv65.Checked == true)
+				{
+					web.FindElement(By.XPath("//input[@id='acc-input']")).SendKeys(rah65);
+				}
+
+				if (kv64.Checked == true)
+				{
+					web.FindElement(By.XPath("//input[@id='lastname-input']")).SendKeys(FIO64);
+				}
+				else if (kv65.Checked == true)
+				{
+					web.FindElement(By.XPath("//input[@id='lastname-input']")).SendKeys(FIO65);
+				}
+
+
+                web.FindElement(By.XPath("//button[@class='btn btn-primary']")).Click();
+				if (kv64.Checked == true)
+				{
+					web.FindElement(By.LinkText("Прибор Ultraheat T 230 ИД.ПУ: 1891039")).Click();
+				}
+				else if (kv65.Checked == true)
+				{
+					web.FindElement(By.LinkText("Прибор Ultraheat T 230 ИД.ПУ: 1891175")).Click();
+				}
+
+				
+
+				web.FindElement(By.XPath("//input[@id='LastDtPok']")).SendKeys(date.Text);//1
+				web.FindElement(By.XPath("//input[@id='LastValuePok']")).SendKeys(q2.Text);//2
+				web.FindElement(By.XPath("//input[@id='LastRashPok']")).SendKeys(q3.Text);//3
+				web.FindElement(By.XPath("//input[@id='LastTimePok']")).SendKeys(q4.Text);//8
+				web.FindElement(By.XPath("//input[@id='TimeProstoya']")).SendKeys(q5.Text);//9
+				web.FindElement(By.XPath("//input[@id='TimeRash']")).SendKeys(q6.Text);//10
+				web.FindElement(By.XPath("//input[@id='HeatPower']")).SendKeys(q7.Text);//5
+				web.FindElement(By.XPath("//input[@id='HeatRash']")).SendKeys(q8.Text);//4
+				web.FindElement(By.XPath("//input[@id='TempO']")).SendKeys(q9.Text);//7
+				web.FindElement(By.XPath("//input[@id='TempPod']")).SendKeys(q10.Text);//6
+				web.FindElement(By.XPath("//input[@id='ErrorKod']")).SendKeys(q11.Text);//11
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Ошибка");
+			}
+		}
+
+		private void kv65_CheckedChanged(object sender, EventArgs e)
+		{
+			kv64.Checked = false;
+			rah.Text = rah65;
+			FIO.Text = FIO65;
+		}
+
+		private void kv64_CheckedChanged(object sender, EventArgs e)
+		{
+			kv65.Checked = false;
+			rah.Text = rah64;
+			FIO.Text = FIO64;
+		}
+		public static bool conection()
+		{
+			try
+			{
+				WebClient cl = new WebClient();
+				string resp;
+				resp = cl.DownloadString("http://www.google.com");
+				return true;
+			}
+			catch (WebException e)
+			{
+				return false;
+			}
+		}
+	}
+}
